aeba556c44994f0149b6e80be95c9a8d4897cdba
orkait <orkaitsolutions@gmail.com>
2024-04-29 21:09:26 +0530

    ignore commit

--- src/components/hooks/useWindowDimensions.tsx
+++ src/components/hooks/useWindowDimensions.tsx
@@ -1,31 +1,31 @@
 import { useState, useEffect } from "react";
 
 export default function useWindowDimensions() {
-	const hasWindow = typeof window !== "undefined";
+    const hasWindow = typeof window !== "undefined";
 
-	function getWindowDimensions() {
-		const width = hasWindow ? window.innerWidth : null;
-		const height = hasWindow ? window.innerHeight : null;
-		return {
-			width,
-			height,
-		};
-	}
+    function getWindowDimensions() {
+        const width = hasWindow ? window.innerWidth : null;
+        const height = hasWindow ? window.innerHeight : null;
+        return {
+            width,
+            height,
+        };
+    }
 
-	const [windowDimensions, setWindowDimensions] = useState(
-		getWindowDimensions()
-	);
+    const [windowDimensions, setWindowDimensions] = useState(
+        getWindowDimensions()
+    );
 
-	useEffect(() => {
-		if (hasWindow) {
-			function handleResize() {
-				setWindowDimensions(getWindowDimensions());
-			}
+    useEffect(() => {
+        if (hasWindow) {
+            const handleResize = () => {
+                setWindowDimensions(getWindowDimensions());
+            }
 
-			window.addEventListener("resize", handleResize);
-			return () => window.removeEventListener("resize", handleResize);
-		}
-	}, [hasWindow]);
+            window.addEventListener("resize", handleResize);
+            return () => window.removeEventListener("resize", handleResize);
+        }
+    }, [hasWindow]);
 
-	return windowDimensions;
+    return windowDimensions;
 }
